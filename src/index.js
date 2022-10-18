7242de3466e0a42875f0a35e0f94abf8a9f12d33
Minhyeok <danny9kwon@gmail.com>
2022-10-18 16:35:32 +0900

    modified:   node_modules/.cache/.eslintcache 	new file:   node_modules/.cache/babel-loader/1705198ce1571dd80bdda79b77f484c1.json 	new file:   node_modules/.cache/babel-loader/328f1daee602136083deb8f1b724d6a3.json 	new file:   node_modules/.cache/babel-loader/be3832aeff594fc185221907fca06696.json 	new file:   src/chapter_13/FancyBorder.jsx 	new file:   src/chapter_13/WelcomeDialog.jsx 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ import FruitSelect from './chapter_11/FruitSelect';
 import Reservation from './chapter_11/Reservation';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
+import WelcomeDialog from './chapter_13/WelcomeDialog';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Calculator/>
+  <WelcomeDialog/>
 );
 reportWebVitals();
